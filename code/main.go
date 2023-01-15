ceae09edd7fc75c1d690d96833823d62c8499c84
LAPTOP-2M61EN29\go <aaron@163.com>
2023-01-15 21:22:23 +0800

    02_算法分析

--- code/main.go
+++ code/main.go
@@ -1,18 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main()  {
-	result := func1(10)
-
-	fmt.Println("result:", result)
+	a := []int{1,2,5, 6}
+	fmt.Println(reverse2(a))
 }
 
-func func1(n int) int {
-	var result = 1
-	for i := 1; i <= n; i++ {
-		result *= i
+func reverse(arr []int) []int {
+	n := len(arr)
+	var tmp int
+
+	end := n - 1
+	for start := 0; start <= end; start++ {
+		tmp = arr[start]
+		arr[start] = arr[end]
+		arr[end] = tmp
+		end--
 	}
 
-	return result
+	return arr
 }
+
+func reverse2(arr []int) []int {
+	n := len(arr)
+	tmp := make([]int, n)
+
+	for i := n - 1; i >= 0; i-- {
+		tmp[n-1-i] = arr[i]
+	}
+	return tmp
+}
+
+
